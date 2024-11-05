92fa626168f122db5c2c6891fb5a4292e70e70a2
khurramazhar773 <105292494+khurramazhar773@users.noreply.github.com>
2024-11-05 18:17:51 +0500

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "apz";
+$servername = "csoft.live";
+$username = "u793298586_apz";
+$password = "5xgj~y9hT*";
+$dbname = "u793298586_apz";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
